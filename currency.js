dee4eecfc5c0f4c7e8f408ffb5a6c189183dc441
timrooke1991 <timrooke1991@gmail.com>
2018-02-24 13:21:32 +0000

    Testing new handler

--- currency.js
+++ currency.js
@@ -2,13 +2,19 @@
 var fs = require('fs');
 
  exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
+   var contents = fs.readFileSync("public/currency.html");
+
+
+   var html = `<p>Hello ${event}</p>`
+
+
    context.succeed({
      statusCode: 200,
-     body: contents.toString(),
+     // body: contents.toString(),
+     body: html,
      headers: {'Content-Type': 'text/html'}
    });
  };
 
  // A function to grab key typed by the user
- // 
+ //
