8794d14076490890d945e2cfccfe614669f6c708
timrooke1991 <timrooke1991@gmail.com>
2018-02-25 10:48:47 +0000

    NPM install

--- currency.js
+++ currency.js
@@ -3,35 +3,35 @@ var fs = require('fs');
 var request = require('request');
 
 
- exports.get = function(event, context) {
-   // var contents = fs.readFileSync("public/index.html");
+exports.get = function(event, context) {
+  // var contents = fs.readFileSync("public/index.html");
 
-   // load pass API data into it
+  // load pass API data into it
 
-   var ticker = event.pathParameters.currency;
+  var ticker = event.pathParameters.currency;
 
-   request({
-     url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`,
-     method: 'GET',
-     headers: {
-       Accept: 'application/json',
-       'Content-Type': 'application/json'
-     }
-   }, function (error, response, body) {
+  request({
+    url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`,
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }, function (error, response, body) {
 
-     console.log('error: '+ response.statusCode);
-     console.log('response: '+ response);
-     console.log(body);
-     var html = `<h1>${response['DISPLAY']['BTC']['USD']['MARKET']}</h1>`;
+    console.log('error: '+ response.statusCode);
+    console.log('response: '+ response);
+    console.log(body);
+    var html = `<h1>${response['DISPLAY']['BTC']['USD']['MARKET']}</h1>`;
 
-     context.succeed({
-       statusCode: 200,
-       // body: contents.toString(),
-       body: html,
-       headers: {'Content-Type': 'text/html'}
-     });
-   });
- };
+    context.succeed({
+      statusCode: 200,
+      // body: contents.toString(),
+      body: html,
+      headers: {'Content-Type': 'text/html'}
+    });
+  });
+};
 
- // A function to grab key typed by the user
- //
+// A function to grab key typed by the user
+//
