02813f2044f86df54c7fb0d3617769155c7962b0
timrooke1991 <timrooke1991@gmail.com>
2018-02-28 14:45:48 +0000

    Progress: seems to be working. Now, to try with HTML as a string

--- currency.js
+++ currency.js
@@ -13,10 +13,10 @@ exports.get = function(event, context) {
 
   rp(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`)
     .then(function (response) {
-      // var html = `<h1>${ticker}</h1>`;
+      var html = `<h1>${response.DISPLAY.BTC.USD.MARKET}</h1>`;
       context.succeed({
         statusCode: 200,
-        body: JSON.stringify(response),
+        body: html,
         headers: {'Content-Type': 'text/html'}
       });
     })
