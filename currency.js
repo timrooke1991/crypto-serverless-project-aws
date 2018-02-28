58390e14521386ba6082c11ea10eeb038557f7fa
timrooke1991 <timrooke1991@gmail.com>
2018-02-28 09:09:37 +0000

    Node modules required for AWS CodeStar?

--- currency.js
+++ currency.js
@@ -10,26 +10,35 @@ exports.get = function(event, context) {
 
   var ticker = event.pathParameters.currency;
 
-  request({
-    url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }, function (error, response, body) {
+  // ERROR - can not find request
+  // request({
+  //   url: `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`,
+  //   method: 'GET',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json'
+  //   }
+  // }, function (error, response, body) {
+  //
+  //   console.log('error: '+ response.statusCode);
+  //   console.log('response: '+ response);
+  //   console.log(body);
+  //   var html = `<h1>${response['DISPLAY']['BTC']['USD']['MARKET']}</h1>`;
+  //
+  //   context.succeed({
+  //     statusCode: 200,
+  //     // body: contents.toString(),
+  //     body: html,
+  //     headers: {'Content-Type': 'text/html'}
+  //   });
+  // });
 
-    console.log('error: '+ response.statusCode);
-    console.log('response: '+ response);
-    console.log(body);
-    var html = `<h1>${response['DISPLAY']['BTC']['USD']['MARKET']}</h1>`;
-
-    context.succeed({
-      statusCode: 200,
-      // body: contents.toString(),
-      body: html,
-      headers: {'Content-Type': 'text/html'}
-    });
+  var html = `<h1>${ticker}</h1>`;
+  context.succeed({
+    statusCode: 200,
+    // body: contents.toString(),
+    body: html,
+    headers: {'Content-Type': 'text/html'}
   });
 };
 
