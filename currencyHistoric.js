d01c50de49396a5e2fac3ab4974fccf603e085d1
timrooke1991 <timrooke1991@gmail.com>
2018-03-04 10:11:41 +0000

    Tweaked promises

--- currencyHistoric.js
+++ currencyHistoric.js
@@ -42,78 +42,79 @@ exports.get = function(event, context) {
           </a>
         </li>
         `;
-      return rp(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`);
-    })
-    .then(function (response, ticker) {
-      var json = JSON.parse(response);
-      console.log('json', json);
-      console.log('output', output);
-      output += `
-            <li class="tile-job">
-              <a href="#">
-                <div class="tile-primary-content">
-                  <h2>Market</h2>
-                  <p>${json['DISPLAY'][ticker]['USD']['MARKET']}</p>
-                </div>
-                <div class="tile-secondary-content">
-                  <p></p>
-                  <small class="sub-title-green">SUPPLY</small>
-                  <p class="no-margin">${json['DISPLAY'][ticker]['USD']['SUPPLY']}</p>
-                  <small class="sub-title-green">MCAP</small>
-                  <p class="no-margin">${json['DISPLAY'][ticker]['USD']['MKTCAP']}</p>
-                  <small class="sub-title">${json['DISPLAY'][ticker]['USD']['LASTUPDATE']}</small>
-                </div>
-              </a>
-            </li>
-            <li class="tile-facebook">
-              <a href="#">
-                <div class="tile-primary-content">
-                  <h2>24hr Trading</h2>
-                  <p><small class="sub-title-blue">CHANGE</small>${json['DISPLAY'][ticker]['USD']['CHANGE24HOUR']} | ${json['DISPLAY'][ticker]['USD']['CHANGEPCT24HOUR']}</p>
-                </div>
-                <div class="tile-secondary-content">
-                  <p><small class="sub-title-blue">OPEN</small>${json['DISPLAY'][ticker]['USD']['OPEN24HOUR']}</p>
-                  <p><small class="sub-title-blue">HIGH</small>${json['DISPLAY'][ticker]['USD']['HIGH24HOUR']}</p>
-                  <p><small class="sub-title-blue">LOW</small>${json['DISPLAY'][ticker]['USD']['LOW24HOUR']}</p>
-                  <p><small class="sub-title-blue">VOLUME</small>${json['DISPLAY'][ticker]['USD']['VOLUME24HOUR']}</p>
-                </div>
-              </a>
-            </li>
-            <li class="tile-twitter">
-              <a href="https://twitter.com/chrisgrabinski">
-                <div class="tile-primary-content">
-                  <h2>Twitter</h2>
-                  <p>@chrisgrabinski Lol. What's wrong with your CSS, bro?</p>
-                </div>
-                <div class="tile-secondary-content">
-                  <p>Follow us on Twitter</p>
-                </div>
-              </a>
-            </li>
-            <li class="tile-person">
-              <a href="#">
-                <div class="tile-primary-content">
-                  <img src="https://pbs.twimg.com/profile_images/687989817171288064/Y4PzAmSn.jpg" alt="">
-                </div>
-                <div class="tile-secondary-content">
-                  <div class="tile-secondary-container">
-                    <h2>Chris Grabinski</h2>
-                    <p>Front-end Development</p>
-                  </div>
-                </div>
-              </a>
-            </li>
-            <li class="tile-client">
-              <a href="#">
-                <div class="tile-primary-content">
-                  <h1 class="ticker-sym">${json['DISPLAY'][ticker]['USD']['FROMSYMBOL']}</h1>
-                </div>
-              </a>
-            </li>
-                </ul>
-          </body>
-          </html>
-          `;
+
+      return rp(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${ticker}&tsyms=USD`)
+        .then(function (response, ticker) {
+          var json = JSON.parse(response);
+          console.log('json', json);
+          console.log('output', output);
+          output += `
+                <li class="tile-job">
+                  <a href="#">
+                    <div class="tile-primary-content">
+                      <h2>Market</h2>
+                      <p>${json['DISPLAY'][ticker]['USD']['MARKET']}</p>
+                    </div>
+                    <div class="tile-secondary-content">
+                      <p></p>
+                      <small class="sub-title-green">SUPPLY</small>
+                      <p class="no-margin">${json['DISPLAY'][ticker]['USD']['SUPPLY']}</p>
+                      <small class="sub-title-green">MCAP</small>
+                      <p class="no-margin">${json['DISPLAY'][ticker]['USD']['MKTCAP']}</p>
+                      <small class="sub-title">${json['DISPLAY'][ticker]['USD']['LASTUPDATE']}</small>
+                    </div>
+                  </a>
+                </li>
+                <li class="tile-facebook">
+                  <a href="#">
+                    <div class="tile-primary-content">
+                      <h2>24hr Trading</h2>
+                      <p><small class="sub-title-blue">CHANGE</small>${json['DISPLAY'][ticker]['USD']['CHANGE24HOUR']} | ${json['DISPLAY'][ticker]['USD']['CHANGEPCT24HOUR']}</p>
+                    </div>
+                    <div class="tile-secondary-content">
+                      <p><small class="sub-title-blue">OPEN</small>${json['DISPLAY'][ticker]['USD']['OPEN24HOUR']}</p>
+                      <p><small class="sub-title-blue">HIGH</small>${json['DISPLAY'][ticker]['USD']['HIGH24HOUR']}</p>
+                      <p><small class="sub-title-blue">LOW</small>${json['DISPLAY'][ticker]['USD']['LOW24HOUR']}</p>
+                      <p><small class="sub-title-blue">VOLUME</small>${json['DISPLAY'][ticker]['USD']['VOLUME24HOUR']}</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="tile-twitter">
+                  <a href="https://twitter.com/chrisgrabinski">
+                    <div class="tile-primary-content">
+                      <h2>Twitter</h2>
+                      <p>@chrisgrabinski Lol. What's wrong with your CSS, bro?</p>
+                    </div>
+                    <div class="tile-secondary-content">
+                      <p>Follow us on Twitter</p>
+                    </div>
+                  </a>
+                </li>
+                <li class="tile-person">
+                  <a href="#">
+                    <div class="tile-primary-content">
+                      <img src="https://pbs.twimg.com/profile_images/687989817171288064/Y4PzAmSn.jpg" alt="">
+                    </div>
+                    <div class="tile-secondary-content">
+                      <div class="tile-secondary-container">
+                        <h2>Chris Grabinski</h2>
+                        <p>Front-end Development</p>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+                <li class="tile-client">
+                  <a href="#">
+                    <div class="tile-primary-content">
+                      <h1 class="ticker-sym">${json['DISPLAY'][ticker]['USD']['FROMSYMBOL']}</h1>
+                    </div>
+                  </a>
+                </li>
+                    </ul>
+              </body>
+              </html>
+              `;
+        });
     })
     .then(function(output) {
       context.succeed({
